249b98f1d0421a4fc9a972367fd75a9b8349e860
Raden Dhaneswara <bennyscar23@gmail.com>
2022-09-14 17:03:10 +0700

    final product before deploy

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+data_barang_katalog = CatalogItem.objects.all()
+context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Raden D'
+}
+
 
 # TODO: Create your views here.
